360c7335f52a3111d1a44892d37faaceeb5ea0e2
Christian de la Cruz <chrisdlc119@outlook.com>
2021-04-25 16:02:35 -0500

    sdm660-common: Bringup PEX

--- sdm660.mk
+++ sdm660.mk
@@ -386,7 +386,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(COMMON_PATH)/overlay \
-    $(COMMON_PATH)/overlay-ssos \
+    $(COMMON_PATH)/overlay-pex \
     $(COMMON_PATH)/overlay-lineage
 
 # RRO configuration
